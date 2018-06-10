168cf1737ca77f0e2c01e7c6324e72c988f94064
Atermonera <atermonera@gmail.com>
2018-06-10 11:54:32 -0700

    Database insertion

--- html/randommeme.php
+++ html/randommeme.php
@@ -1,3 +1,6 @@
+<?php
+		include 'sessionheader.php';
+?>
 <!DOCTYPE html>
 <?php
 		$currentpage="FindMyMeme";
@@ -12,6 +15,9 @@
 	<body>
 
 		<?php
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
 		// change the value of $dbuser and $dbpass to your username and password
 			include 'connectvars.php'; 
 			include 'header.php';	
@@ -34,19 +40,21 @@
 				$MemeID 	= $row["MemeID"];
 				$CaptionID 	= $row["CaptionID"];
 				
-		// 		$username = $_SESSION["user"];
-		// 		$query2 = "SELECT COUNT() as C FROM (SELECT * FROM Questionnaire WHERE Username = $username) as A";
-		// 		if($result2 = msqli_query($conn, $query)){
-		// 			$resultrow = msqli_fetch_assoc($result2);
-		// 			$QuestionnaireID = $resultrow["C"];
-		// 			msqli_free_result($result2);
+				if(isset($_SESSION["user"])){
+			 		$username = $_SESSION["user"];
+		 			$query2 = "SELECT COUNT(*) as C FROM (SELECT * FROM Questionnaire WHERE Username = '$username') as A";
+		 			if($result2 = mysqli_query($conn, $query2)){
+		 				$resultrow = mysqli_fetch_assoc($result2);
+		 				$QuestionnaireID = $resultrow['C'];
 			
-		// 			Insert username, questionnaireID, memeid, captionid into questionnaire
-		// 			$query2 = "INSERT INTO Questionnaire (Username, MemeID, CaptionID, QuestionnaireID) VALUES ($username, $MemeID, $CaptionID, $QuestionnaireID)";
-		//	} else {
-		//		die("Query to insert matched meme failed");
-		//	}
-				
+					// Insert username, questionnaireID, memeid, captionid into questionnaire
+		 				$query2 = "INSERT INTO Questionnaire (Username, MemeID, CaptionID, QuestionnaireID) VALUES ('$username', '$MemeID', '$CaptionID', '$QuestionnaireID')";
+						if(!mysqli_query($conn, $query2)){
+							echo "Error: " . $query2 . "<br>" . mysqli_error($conn);
+						}
+					}
+					mysqli_free_result($result2);
+				}
 				echo "<img src='".$url."' alt='Cover' style='max-width:25%;display:block;margin-left:auto;margin-right:auto'><div style='margin:auto;margin-top:10px;width:20%;text-align:center'>".$caption."</div>";
 			}
 
