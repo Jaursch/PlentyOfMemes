71215bd624eefe3ddd9c12a4e5a8ad437426ae54
Burton Jaursch <jaurschb@oregonstate.edu>
2018-06-10 16:28:22 -0700

    headersession.php added to home.php. Weird issue

--- html/home.php
+++ html/home.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <?php
+		include 'sessionheader.php';
 		$currentpage="Home";
 		include "pages.php";
 ?>
@@ -8,33 +9,33 @@
 		<title>MemeMe Home</title>
 		<link rel="stylesheet" href="spectre.min.css">
 	</head>
-	
+
 	<body>
 
 		<?php
 		// change the value of $dbuser and $dbpass to your username and password
-			include 'connectvars.php'; 
-			include 'header.php';	
+			include 'connectvars.php';
+			include 'header.php';
 
 			$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 			if (!$conn) {
 				die('Could not connect: ' . mysql_error());
-			}	
+			}
 
 		// query to select all information from supplier table
 			$query = "SELECT * FROM Meme INNER JOIN Caption USING(MemeID) ORDER BY RAND() LIMIT 16;";
-	
+
 		// Get results from query
 			$result = mysqli_query($conn, $query);
 			if (!$result) {
 				die("Query to show fields from table failed");
 			}
 
-		// get number of columns in table	
+		// get number of columns in table
 			$fields_num = mysqli_num_fields($result);
 			echo "<h1>Daily Memes:</h1>";
 			echo "<table class='table'><tr>";
-	
+
 		// printing table headers
 			for($i=0; $i < 4; $i++){
 				echo "<tr>";
